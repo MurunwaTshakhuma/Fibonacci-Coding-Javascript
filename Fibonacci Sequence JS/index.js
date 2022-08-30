d08813561ebd8722656a0c11f40a2bc7eb3791e8
Murunwa Tshakhuma <103765994+MurunwaTshakhuma@users.noreply.github.com>
2022-08-30 04:18:25 +0200

    Update index.js

--- Fibonacci Sequence JS/index.js
+++ Fibonacci Sequence JS/index.js
@@ -1,27 +1,18 @@
 function fibonacciGenerator (n) {
-
-  var a=0;
-  var b=1;
-  var temp;
   var output=[];  
   if(n===1){
-      output.push(a);
+      output.push(0);
     } //Control statement for n=1 items.
     
   else if(n==2){
-      output.push(a);
-      output.push(b);
+      output.push(0);
+      output.push(1);
   }//Control statement for n=1 items.
     
   else 
-    
-    output.push(a);
-    output.push(b);
+      output=[0,1];
     for(var i=2;i<n;i++){
-      output.push(a+b);
-      temp=b;
-      b=a+b;
-      a=temp;
+      output.push(output[output.length-2]+output[output.length-1]);
     } //Control statement for more than 2 items in the sequence.
       
 return console.log(output);
